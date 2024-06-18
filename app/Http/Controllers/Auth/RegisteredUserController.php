c5e4cbf87eb2f7dfa97e564fcffcd17eaa5538df
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-06-18 19:41:35 +0700

    set registercontroller, User, add form for occupation & avavatar

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,13 +31,22 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'occupation' => ['required', 'string', 'max:255'],
+            'avatar' => ['required', 'image', 'mimes:png,jpg,jpeg', 'max:2048'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        if ($request->hasFile('avatar')) {
+            $avatarPath = $request->file('avatar')->store('avatars', 'public');
+        } else {
+            $avatarPath = 'images/avatar-default.png';
+        }
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'occupation' => $request->occupation,
+            'avatar' => $request->avatarPath,
             'password' => Hash::make($request->password),
         ]);
 
