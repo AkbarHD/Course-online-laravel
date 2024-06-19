262e22fa22ba7f0be43d22d4ff1e3d9f866ca613
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-06-19 16:07:11 +0700

    add routing for role student

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CourseVideoController;
 use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SubscribeTransactionController;
@@ -27,14 +28,23 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/checkout', [FrontController::class, 'checkout'])->name('front.checkout');
-    Route::post('/checkout/store', [FrontController::class, 'checkout_store'])->name('front.checkout');
+    Route::get('/checkout', [FrontController::class, 'checkout'])->name('front.checkout')->middleware('role:student');
+    Route::post('/checkout/store', [FrontController::class, 'checkout_store'])->name('front.checkout')->middleware('role:student');
+
+    Route::get('/learning/{course}/{courseVideoId}', [FrontController::class, 'learning'])->name('front.learning')
+        ->middleware('role:student|teacher|owner');
 
     Route::prefix('admin')->name('admin.')->group(function () {
+        // crud categories
         Route::resource('categories', CategoryController::class)->middleware('role:owner');
         Route::resource('teachers', TeacherController::class)->middleware('role:owner');
+        // crud courses
         Route::resource('courses', CourseController::class)->middleware('role:owner|teacher');
+
         Route::resource('subscribe_transactions', SubscribeTransactionController::class)->middleware('role:owner');
+
+        Route::get('/add/video/{course:id}', [CourseVideoController::class, 'create'])->middleware('role:teacher|owner')->name('course.add_video');
+        Route::get('/add/video/save/{course:id}', [CourseVideoController::class, 'store'])->middleware('role:teacher|owner')->name('course.add_video.save');
         Route::resource('course_videos', CourseVideo::class)->middleware('role:owner|teacher');
     });
 });
