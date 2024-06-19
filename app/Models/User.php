0c2050b11d2078dc71b4658c3faff7e7d03e5e50
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-06-19 10:53:37 +0700

    check user subscription

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -51,5 +53,23 @@ class User extends Authenticatable
     public function courses()
     {
         return $this->belongsToMany(Course::class, 'course_students');
+    }
+
+    public function subscribe_transactions()
+    {
+        return $this->hasMany(SubscribeTransaction::class);
+    }
+
+    public function hasActiveSubscription()
+    {
+        $latestSubscription = $this->subscribe_transactions()->where('is_paid', true)
+            ->latest('updated_at')->first();
+
+        if (!$latestSubscription) {
+            return false;
+        }
+
+        $subscriptionEndDate = Carbon::parse($latestSubscription->subscription_start_date)->addMonths(1);
+        return Carbon::now()->lessThanOrEqualTo($subscriptionEndDate);
     }
 }
