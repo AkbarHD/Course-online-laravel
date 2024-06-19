fe9e12c597f74a34be797a66dcf4800abe3fac6e
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-06-19 15:26:56 +0700

    uji coba register as student

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,7 +46,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'occupation' => $request->occupation,
-            'avatar' => $request->avatarPath,
+            'avatar' => $avatarPath,
             'password' => Hash::make($request->password),
         ]);
 
