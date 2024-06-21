3556795063c57ae053f91f9315301487f144d0bc
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-06-21 10:48:14 +0700

    add dan delete teacher

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTeacherRequest;
 use App\Models\Teacher;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class TeacherController extends Controller
 {
@@ -12,7 +16,8 @@ class TeacherController extends Controller
      */
     public function index()
     {
-        //
+        $teachers = Teacher::orderBy('id', 'DESC')->get();
+        return view('admin.teachers.index', compact('teachers'));
     }
 
     /**
@@ -20,15 +25,42 @@ class TeacherController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.teachers.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTeacherRequest $request)
     {
-        //
+        $validated = $request->validated();
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user) {
+            return back()->withErrors([
+                'email' => 'Email anda tidak terdaftar',
+            ]);
+        }
+
+        if ($user->hasAnyRole('teacher')) {
+            return back()->withErrors([
+                'email' => 'Email ini sudah terdaftar sebagai guru',
+            ]);
+        }
+
+        DB::transaction(function () use ($user, $validated) {
+            $validated['user_id'] = $user->id;
+            $validated['is_active'] = true;
+
+            Teacher::create($validated);
+
+            if ($user->hasRole('student')) {
+                $user->removeRole('student');
+            }
+            $user->assignRole('teacher');
+        });
+
+        return redirect()->route('admin.teachers.index');
     }
 
     /**
@@ -60,6 +92,18 @@ class TeacherController extends Controller
      */
     public function destroy(Teacher $teacher)
     {
-        //
+        try {
+            $teacher->delete();
+            $user = User::find($teacher->user_id);
+            $user->removeRole('teacher');
+            $user->assignRole('student');
+            return redirect()->route('admin.teachers.index');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            $error = ValidationException::withMessages([
+                'system_error' => ['system error' . $e->getMessage()],
+            ]);
+            throw $error;
+        }
     }
 }
--- resources/views/admin/teachers/create.blade.php
+++ resources/views/admin/teachers/create.blade.php
@@ -9,25 +9,26 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">
 
-                @if($errors->any())
-                    @foreach($errors->all() as $error)
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error)
                         <div class="py-3 w-full rounded-3xl bg-red-500 text-white">
-                            {{$error}}
+                            {{ $error }}
                         </div>
                     @endforeach
                 @endif
-                
-                <form method="POST" action="#" enctype="multipart/form-data">
+
+                <form method="POST" action="{{ route('admin.teachers.store') }}" enctype="multipart/form-data">
                     @csrf
 
                     <div>
                         <x-input-label for="email" :value="__('email')" />
-                        <x-text-input id="email" class="block mt-1 w-full" type="text" email="text" :value="old('email')" required autofocus autocomplete="email" />
+                        <x-text-input id="email" class="block mt-1 w-full" name="email" type="email"
+                            :value="old('email')" required autofocus autocomplete="email" />
                         <x-input-error :messages="$errors->get('email')" class="mt-2" />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
-            
+
                         <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
                             Add New Teacher
                         </button>
